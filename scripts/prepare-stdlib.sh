91363a6719d74c59dc87ea5418872cdaf763ec80
VinaLx <vinaleux@gmail.com>
2018-03-26 22:24:58 +0800

    test travis 8

--- scripts/prepare-stdlib.sh
+++ scripts/prepare-stdlib.sh
@@ -2,7 +2,7 @@ mkdir ~/.agda && cd ~/.agda || exit 1
 
 pwd
 
-agda --version
-
 git clone https://github.com/agda/agda-stdlib.git
+
+echo "$HOME/.agda/agda-stdlib/standard-library.agda-lib" > libraries
 
